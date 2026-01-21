729d43189f3379e5e072ecbd51ae97a97bd5ad5f
lukas.andel <workinnet.la@gmail.com>
2026-01-21 00:05:00 +0000

    72h deploy 2026-01-21_000500

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "72h-cache-v20260121-01";
+const CACHE = "72h-cache-v20260121-02";
 const ASSETS=["./", "./index.html", "./app.js", "./faq.json", "./synonyms.json", "./help.html", "./offline.html", "./manifest.webmanifest", "./sw.js", "./icons/icon.svg", "./icons/icon-192.png", "./icons/icon-512.png"];
 self.addEventListener("install",(e)=>e.waitUntil((async()=>{const c=await caches.open(CACHE);await c.addAll(ASSETS);self.skipWaiting();})()));
 self.addEventListener("activate",(e)=>e.waitUntil((async()=>{const ks=await caches.keys();await Promise.all(ks.map(k=>k===CACHE?null:caches.delete(k)));self.clients.claim();})()));
